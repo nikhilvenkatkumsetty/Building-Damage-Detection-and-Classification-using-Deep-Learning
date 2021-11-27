1ac5f03dfda5765cceaa92d30c74a766e485775e
Nikhil Venkat <nikhilvenkat26@gmail.com>
2021-11-27 13:47:38 +0530

    initial

---
+++ Earthquake_Damage_Detection/README.md
@@ -0,0 +1,2 @@
+# Earthquake Damage Detection
+Buildings segmentation from satellite imagery and damage classification for each build
---
+++ Earthquake_Damage_Detection/utils.py
@@ -0,0 +1,22 @@
+from imageproc import Image
+import numpy as np
+from tqdm import tqdm
+import os
+
+def get_training_set(image_path, mask_path):
+    ids = os.listdir(image_path)
+    X = []
+    Y = []
+  
+    for i in tqdm(ids):
+        img = Image('{}/{}'.format(image_path, i), 'image')
+        #img.normalize()
+        img.set_band_type('last')
+        msk = Image('{}/{}'.format(mask_path, i), 'mask')
+        msk.set_band_type('last')
+        X.append(img.get_array())
+        Y.append(msk.get_array())
+
+    X = np.array(X)
+    X = X/X.max()  
+    return np.array(X), np.array(Y)
